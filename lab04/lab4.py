a9a5dbc11e23db98408acfef44ba344d0322d25e
Иван Гуров <quistandd@gmail.com>
2026-03-03 22:15:36 +0700

    Добавлен код

---
+++ lab04/lab4.py
@@ -0,0 +1,53 @@
+import random
+import math
+
+class LCG:
+    def __init__(self, seed=1):
+        self.a = 1664525
+        self.c = 1013904223
+        self.m = 2**32
+        self.state = seed
+
+    def random(self):
+        self.state = (self.a * self.state + self.c) % self.m
+        return self.state / self.m  # нормировка к [0,1)
+
+
+def sample_mean(data):
+    return sum(data) / len(data)
+
+def sample_variance(data):
+    mean = sample_mean(data)
+    return sum((x - mean) ** 2 for x in data) / len(data)
+
+
+N = 100_000
+
+
+lcg = LCG(seed=42)
+data_lcg = [lcg.random() for _ in range(N)]
+
+mean_lcg = sample_mean(data_lcg)
+var_lcg = sample_variance(data_lcg)
+
+
+data_builtin = [random.random() for _ in range(N)]
+
+mean_builtin = sample_mean(data_builtin)
+var_builtin = sample_variance(data_builtin)
+
+
+
+print("Размер выборки:", N)
+print()
+
+print("LCG генератор:")
+print("Среднее:", mean_lcg)
+print("Дисперсия:", var_lcg)
+print()
+
+print("Встроенный генератор:")
+print("Среднее:", mean_builtin)
+print("Дисперсия:", var_builtin)
+print()
+
